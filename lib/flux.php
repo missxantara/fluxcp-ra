0e2ffb1df025708dcf6ce78d3ac30c26f1d933da
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-08-07 08:49:42 +0000

    Added skeletal login method

--- lib/flux.php
+++ lib/flux.php
@@ -3,5 +3,15 @@ class Flux {
 	public static $config;
 	public static $servers = array();
 	private function __construct(){}
+	
+	public static function login(LoginServer $loginServer, $username, $password)
+	{
+		if ($loginServer->isAuth($username, $password)) {
+			
+		}
+		else {
+			
+		}
+	}
 }
 ?>
