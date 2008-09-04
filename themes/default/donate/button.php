b7c7a8e00747d82f194cb5f9468eb8f8d1b8cff6
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-04 06:28:17 +0000

    Added PayPal donation support.

---
+++ themes/default/donate/button.php
@@ -0,0 +1,17 @@
+<?php
+$businessEmail = htmlspecialchars(Flux::config('PayPalBusinessEmail'));
+$itemName = htmlspecialchars('Donation credits (1 credit per $'.$this->formatDollar(Flux::config('CreditExchangeRate')).' USD)');
+?>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="<?php echo $businessEmail ?>">
+<input type="hidden" name="item_name" value="<?php echo $itemName ?>">
+<input type="hidden" name="no_shipping" value="0">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="tax" value="0">
+<input type="hidden" name="lc" value="US">
+<input type="hidden" name="bn" value="PP-DonationsBF">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
