6d6dee85b993a1ac06c8eb904df14ccfa9d95591
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-05 06:30:03 +0000

    Added online player count and removed some old unused code.

--- modules/server/status.php
+++ modules/server/status.php
@@ -9,10 +9,17 @@ foreach (Flux::$loginAthenaGroupRegistry as $groupName => $loginAthenaGroup) {
 	
 	foreach ($loginAthenaGroup->athenaServers as $athenaServer) {
 		$serverName = $athenaServer->serverName;
+		
+		$sql = "SELECT COUNT(char_id) AS players_online FROM {$athenaServer->charMapDatabase}.char WHERE online > 0";
+		$sth = $loginAthenaGroup->connection->getStatement($sql);
+		$sth->execute();
+		$res = $sth->fetch();
+		
 		$serverStatus[$groupName][$serverName] = array(
 			'loginServerUp' => $loginServerUp,
 			 'charServerUp' => $athenaServer->charServer->isUp(),
-			  'mapServerUp' => $athenaServer->mapServer->isUp()
+			  'mapServerUp' => $athenaServer->mapServer->isUp(),
+			'playersOnline' => intval($res ? $res->players_online : 0)
 		);
 	}
 }
--- themes/default/server/status.php
+++ themes/default/server/status.php
@@ -1,37 +1,15 @@
 <h2>Server Status</h2>
-<table id="server_status" style="display: none">
-<?php foreach ($serverStatus as $privServerName => $gameServers): ?>
-	<tr>
-		<td colspan="3" style="font-size: 18pt"><?php echo htmlspecialchars($privServerName) ?></td>
-	</tr>
-	<?php foreach ($gameServers as $serverName => $gameServer): ?>
-	<tr>
-		<th rowspan="3" style="padding-left: 10px"><?php echo htmlspecialchars($serverName) ?></th>
-		<td>Login</td>
-		<td><?php echo $this->serverUpDown($gameServer['loginServerUp']) ?></td>
-	</tr>
-	<tr>
-		<td>Char</td>
-		<td><?php echo $this->serverUpDown($gameServer['charServerUp']) ?></td>
-	</tr>
-	<tr>
-		<td>Map</td>
-		<td><?php echo $this->serverUpDown($gameServer['mapServerUp']) ?></td>
-	</tr>
-	<?php endforeach ?>
-<?php endforeach ?>
-</table>
-
 <table id="server_status">
 <?php foreach ($serverStatus as $privServerName => $gameServers): ?>
 	<tr>
-		<td colspan="4"><h3><?php echo htmlspecialchars($privServerName) ?></h3></td>
+		<td colspan="5"><h3><?php echo htmlspecialchars($privServerName) ?></h3></td>
 	</tr>
 	<tr>
 		<th>Servers</th>
 		<td class="status">Login</td>
 		<td class="status">Char</td>
 		<td class="status">Map</td>
+		<td class="status">Online</td>
 	</tr>
 	<?php foreach ($gameServers as $serverName => $gameServer): ?>
 	<tr>
@@ -39,6 +17,7 @@
 		<td class="status"><?php echo $this->serverUpDown($gameServer['loginServerUp']) ?></td>
 		<td class="status"><?php echo $this->serverUpDown($gameServer['charServerUp']) ?></td>
 		<td class="status"><?php echo $this->serverUpDown($gameServer['mapServerUp']) ?></td>
+		<td class="status"><?php echo $gameServer['playersOnline'] ?></td>
 	</tr>
 	<?php endforeach ?>
 <?php endforeach ?>
