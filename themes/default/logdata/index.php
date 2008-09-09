8ac607ffb5e7f9e69e5f9d17db4812607d1bdf71
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-09 01:44:15 +0000

    Fixed typo.

--- themes/default/logdata/index.php
+++ themes/default/logdata/index.php
@@ -1,3 +1,3 @@
 <h2>Log Data</h2>
 <p>You may view the server logs here.</p>
-<p>Please select the logs that you would like to view from the available actions.</p>
+<p>Please select the log that you would like to view from the available actions.</p>
