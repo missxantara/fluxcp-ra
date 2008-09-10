6fb8257cccc31bf9ff9ab95eb141ec8ea7d332a1
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-10 16:26:08 +0000

    Added BANNED and PERMABANNED login error constants.

--- lib/Flux/LoginError.php
+++ lib/Flux/LoginError.php
@@ -5,5 +5,7 @@ class Flux_LoginError extends Flux_Error {
 	const UNEXPECTED     = 0;
 	const INVALID_SERVER = 1;
 	const INVALID_LOGIN  = 2;
+	const BANNED         = 3;
+	const PERMABANNED    = 4;
 }
 ?>
