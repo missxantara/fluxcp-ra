2bb39fbd624e414466988a0ff4f21dbe6d71e80c
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-12 05:28:49 +0000

    Fixed donate button causing layout inconsistencies and also centered the button by default.

--- themes/default/donate/button.php
+++ themes/default/donate/button.php
@@ -9,17 +9,18 @@ $creditExchangeRate = Flux::config('CreditExchangeRate');
 $itemName           = htmlspecialchars('Donation Credits (1 credit per '.$this->formatDollar($creditExchangeRate).' '.$donationCurrency.')');
 ?>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_donations">
-<input type="hidden" name="return" value="<?php echo $this->entireUrl(false) ?>">
-<input type="hidden" name="custom" value="<?php echo $customDataEscaped ?>">
-<input type="hidden" name="business" value="<?php echo $businessEmail ?>">
-<input type="hidden" name="item_name" value="<?php echo $itemName ?>">
-<input type="hidden" name="no_shipping" value="0">
-<input type="hidden" name="no_note" value="1">
-<input type="hidden" name="currency_code" value="<?php echo $donationCurrency ?>">
-<input type="hidden" name="tax" value="0">
-<input type="hidden" name="lc" value="US">
-<input type="hidden" name="bn" value="PP-DonationsBF">
-<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+<input type="hidden" name="cmd" value="_donations" />
+<input type="hidden" name="return" value="<?php echo $this->entireUrl(false) ?>" />
+<input type="hidden" name="custom" value="<?php echo $customDataEscaped ?>" />
+<input type="hidden" name="business" value="<?php echo $businessEmail ?>" />
+<input type="hidden" name="item_name" value="<?php echo $itemName ?>" />
+<input type="hidden" name="no_shipping" value="0" />
+<input type="hidden" name="no_note" value="1" />
+<input type="hidden" name="currency_code" value="<?php echo $donationCurrency ?>" />
+<input type="hidden" name="tax" value="0" />
+<input type="hidden" name="lc" value="US" />
+<input type="hidden" name="bn" value="PP-DonationsBF" />
+<p style="text-align: center"><input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif"
+	name="submit" alt="PayPal - The safer, easier way to pay online!" /></p>
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
 </form>
