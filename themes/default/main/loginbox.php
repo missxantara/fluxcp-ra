9f4f458e467a3b518f10f72f8a51d7c1ab01b135
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-12 09:21:40 +0000

    Darkened the login info box.

--- themes/default/main/loginbox.php
+++ themes/default/main/loginbox.php
@@ -2,13 +2,13 @@
 <?php if ($session->isLoggedIn()): ?>
 <table cellspacing="0" cellpadding="0" width="100%" id="loginbox">
 	<tr>
-		<td width="18"><img src="<?php echo $this->themePath('img/content_tl.gif') ?>" alt="[CONTENT_TOP_LEFT]" /></td>
-		<td bgcolor="#f8f8f8"></td>
-		<td width="18"><img src="<?php echo $this->themePath('img/content_tr.gif') ?>" alt="[CONTENT_TOP_RIGHT]" /></td>
+		<td width="18"><img src="<?php echo $this->themePath('img/loginbox_tl.gif') ?>" /></td>
+		<td bgcolor="#e1eaf3"></td>
+		<td width="18"><img src="<?php echo $this->themePath('img/loginbox_tr.gif') ?>" /></td>
 	</tr>
 	<tr>
-		<td bgcolor="#f8f8f8"></td>
-		<td bgcolor="#f8f8f8" valign="middle">
+		<td bgcolor="#e1eaf3"></td>
+		<td bgcolor="#e1eaf3" valign="middle">
 			<span style="display: inline-block; margin: 2px 2px 2px 0">
 				You are currently logged in as <strong><a href="<?php echo $this->url('account', 'view') ?>" title="View account"><?php echo htmlspecialchars($session->account->userid) ?></a></strong>
 				on <?php echo htmlspecialchars($session->serverName) ?>.  Your preferred server is:
@@ -22,12 +22,12 @@
 				<input type="hidden" name="preferred_server" value="">
 			</form>
 		</td>
-		<td bgcolor="#f8f8f8"></td>
+		<td bgcolor="#e1eaf3"></td>
 	</tr>
 	<tr>
-		<td><img src="<?php echo $this->themePath('img/content_bl.gif') ?>" alt="[CONTENT_BOTTOM_LEFT]" /></td>
-		<td bgcolor="#f8f8f8"></td>
-		<td><img src="<?php echo $this->themePath('img/content_br.gif') ?>" alt="[CONTENT_BOTTOM_RIGHT]" /></td>
+		<td><img src="<?php echo $this->themePath('img/loginbox_bl.gif') ?>" /></td>
+		<td bgcolor="#e1eaf3"></td>
+		<td><img src="<?php echo $this->themePath('img/loginbox_br.gif') ?>" /></td>
 	</tr>
 </table>
 <?php endif ?>
