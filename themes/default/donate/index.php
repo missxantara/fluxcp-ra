3a418b3cc057bb96f20153ab3bf7e6db36224b76
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-17 17:08:16 +0000

    Fixed some wording.

--- themes/default/donate/index.php
+++ themes/default/donate/index.php
@@ -1,7 +1,7 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 <h2>Donate</h2>
 <?php if (Flux::config('AcceptDonations')): ?>
-	<p>By donating, you're supporting the costs of <em>running</em> this server and <em>maintaining</em> it.  In return, if you are logged in you will be rewarded <span class="keyword">donation credits</span> that you may use to purchase items from our <a href="<?php echo $this->url('purchase') ?>">item shop</a>.</p>
+	<p>By donating, you're supporting the costs of <em>running</em> this server and <em>maintaining</em> it.  In return, you will be rewarded <span class="keyword">donation credits</span> that you may use to purchase items from our <a href="<?php echo $this->url('purchase') ?>">item shop</a>.</p>
 <h3>Are you ready to donate?</h3>
 	<p>All donations towards us are received by PayPal, but don't worry!  Even if you don't have an account with PayPal, you can still use your credit card to donate!</p>
 	<p>When you're ready to donate, click the big <span class="keyword">Donate</span> button to proceed with your transaction.  You may enter your donation amount from there.
