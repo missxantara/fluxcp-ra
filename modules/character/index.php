1a4ce1348d119a3501d36a299b6950009668a40b
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-17 07:59:14 +0000

    Added character search features.

--- modules/character/index.php
+++ modules/character/index.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined('FLUX_ROOT')) exit;
 
+$bind        = array();
 $sqlpartial  = "LEFT OUTER JOIN {$server->charMapDatabase}.guild_member ON guild_member.char_id = ch.char_id ";
 $sqlpartial .= "LEFT OUTER JOIN {$server->charMapDatabase}.guild ON guild.guild_id = guild_member.guild_id ";
 $sqlpartial .= "LEFT OUTER JOIN {$server->loginDatabase}.login ON login.account_id = ch.account_id ";
@@ -8,11 +9,142 @@ $sqlpartial .= "LEFT OUTER JOIN {$server->charMapDatabase}.`char` AS partner ON 
 $sqlpartial .= "LEFT OUTER JOIN {$server->charMapDatabase}.`char` AS mother ON mother.char_id = ch.mother ";
 $sqlpartial .= "LEFT OUTER JOIN {$server->charMapDatabase}.`char` AS father ON father.char_id = ch.father ";
 $sqlpartial .= "LEFT OUTER JOIN {$server->charMapDatabase}.`char` AS child ON child.char_id = ch.child ";
+$sqlpartial .= "WHERE 1=1 ";
+
+$charID = $params->get('char_id');
+if ($charID) {
+	$sqlpartial .= "AND ch.char_id = ? ";
+	$bind[]      = $charID;
+}
+else {
+	$opMapping   = array('eq' => '=', 'gt' => '>', 'lt' => '<');
+	$opValues    = array_keys($opMapping);
+	$account     = $params->get('account');
+	$charName    = $params->get('char_name');
+	$charClass   = $params->get('char_class');
+	$baseLevelOp = $params->get('base_level_op');
+	$baseLevel   = $params->get('base_level');
+	$jobLevelOp  = $params->get('job_level_op');
+	$jobLevel    = $params->get('job_level');
+	$zenyOp      = $params->get('zeny_op');
+	$zeny        = $params->get('zeny');
+	$guild       = $params->get('guild');
+	$partner     = $params->get('partner');
+	$mother      = $params->get('mother');
+	$father      = $params->get('father');
+	$child       = $params->get('child');
+	$online      = $params->get('online');
+	$slotOp      = $params->get('slot_op');
+	$slot        = $params->get('slot');
+	
+	if ($account) {
+		if (preg_match('/^\d+$/', $account)) {
+			$sqlpartial .= "AND login.account_id = ? ";
+			$bind[]      = $account;
+		}
+		else {
+			$sqlpartial .= "AND (login.userid LIKE ? OR login.userid = ?) ";
+			$bind[]      = "%$account%";
+			$bind[]      = $account;
+		}
+	}
+	
+	if ($charName) {
+		$sqlpartial .= "AND (ch.name LIKE ? OR ch.name = ?)";
+		$bind[]      = "%$charName%";
+		$bind[]      = $charName;
+	}
+	
+	if ($charClass) {
+		$className = preg_quote($charClass);
+		$classIDs  = preg_grep("/.*?$className.*?/", Flux::config('JobClasses')->toArray());
+		
+		if (count($classIDs)) {
+			$classIDs    = array_keys($classIDs);
+			$sqlpartial .= "AND (";
+			$partial     = '';
+			
+			foreach ($classIDs as $id) {
+				$partial .= "ch.class = ? OR ";
+				$bind[]   = $id;
+			}
+			
+			$partial     = preg_replace('/\s*OR\s*$/', '', $partial);
+			$sqlpartial .= "$partial) ";
+		}
+		else {
+			$sqlpartial .= 'AND ch.class IS NULL ';
+		}
+	}
+	
+	if (in_array($baseLevelOp, $opValues) && trim($baseLevel) != '') {
+		$op          = $opMapping[$baseLevelOp];
+		$sqlpartial .= "AND ch.base_level $op ? ";
+		$bind[]      = $baseLevel;
+	}
+	
+	if (in_array($jobLevelOp, $opValues) && trim($jobLevel) != '') {
+		$op          = $opMapping[$jobLevelOp];
+		$sqlpartial .= "AND ch.job_level $op ? ";
+		$bind[]      = $jobLevel;
+	}
+	
+	if (in_array($zenyOp, $opValues) && trim($zeny) != '') {
+		$op          = $opMapping[$zenyOp];
+		$sqlpartial .= "AND ch.zeny $op ? ";
+		$bind[]      = $zeny;
+	}
+	
+	if ($guild) {
+		$sqlpartial .= "AND (guild.name LIKE ? OR guild.name = ?) ";
+		$bind[]      = "%$guild%";
+		$bind[]      = $guild;
+	}
+	
+	if ($partner) {
+		$sqlpartial .= "AND (partner.name LIKE ? OR partner.name = ?) ";
+		$bind[]      = "%$partner%";
+		$bind[]      = $partner;
+	}
+	
+	if ($mother) {
+		$sqlpartial .= "AND (mother.name LIKE ? OR mother.name = ?) ";
+		$bind[]      = "%$mother%";
+		$bind[]      = $mother;
+	}
+	
+	if ($father) {
+		$sqlpartial .= "AND (father.name LIKE ? OR father.name = ?) ";
+		$bind[]      = "%$father%";
+		$bind[]      = $father;
+	}
+	
+	if ($child) {
+		$sqlpartial .= "AND (child.name LIKE ? OR child.name = ?) ";
+		$bind[]      = "%$child%";
+		$bind[]      = $child;
+	}
+	
+	if ($online == 'on' || $online == 'off') {
+		if ($online == 'on') {
+			$sqlpartial .= "AND ch.online > 0 ";
+		}
+		else {
+			$sqlpartial .= "AND ch.online < 1 ";
+		}
+	}
+	
+	if (in_array($slotOp, $opValues) && trim($slot) != '') {
+		$op          = $opMapping[$slotOp];
+		$sqlpartial .= "AND ch.char_num $op ? ";
+		$bind[]      = $slot - 1;
+	}
+}
 
 $sql  = "SELECT COUNT(ch.char_id) AS total FROM {$server->charMapDatabase}.`char` AS ch $sqlpartial";
 $sth  = $server->connection->getStatement($sql);
 
-$sth->execute();
+$sth->execute($bind);
 $paginator = $this->getPaginator($sth->fetch()->total);
 $paginator->setSortableColumns(array(
 	'ch.char_id' => 'asc', 'userid', 'char_name', 'ch.base_level', 'ch.job_level',
@@ -31,7 +163,7 @@ $sql  = "SELECT $col FROM {$server->charMapDatabase}.`char` AS ch $sqlpartial";
 $sql  = $paginator->getSQL($sql);
 $sth  = $server->connection->getStatement($sql);
 
-$sth->execute();
+$sth->execute($bind);
 
 $characters = $sth->fetchAll();
 ?>
--- themes/default/character/index.php
+++ themes/default/character/index.php
@@ -1,5 +1,81 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 <h2>Characters</h2>
+<form action="<?php echo $this->url ?>" method="get" class="search-form">
+	<p>Search for character(s):</p>
+	<p>
+		<label for="char_id">Character ID:</label>
+		<input type="text" name="char_id" id="char_id" value="<?php echo htmlspecialchars($params->get('char_id')) ?>" />
+		…
+		<label for="account">Account:</label>
+		<input type="text" name="account" id="account" value="<?php echo htmlspecialchars($params->get('account')) ?>" />
+		…
+		<label for="char_name">Character:</label>
+		<input type="text" name="char_name" id="char_name" value="<?php echo htmlspecialchars($params->get('char_name')) ?>" />
+		…
+		<label for="char_class">Job Class:</label>
+		<input type="text" name="char_class" id="char_class" value="<?php echo htmlspecialchars($params->get('char_class')) ?>" />
+	</p>
+	<p>
+		<label for="base_level">Base Level:</label>
+		<select name="base_level_op">
+			<option value="eq"<?php if (($base_level_op=$params->get('base_level_op')) == 'eq') echo ' selected="selected"' ?>>is equal to</option>
+			<option value="gt"<?php if ($base_level_op == 'gt') echo ' selected="selected"' ?>>is greater than</option>
+			<option value="lt"<?php if ($base_level_op == 'lt') echo ' selected="selected"' ?>>is less than</option>
+		</select>
+		<input type="text" name="base_level" id="base_level" value="<?php echo htmlspecialchars($params->get('base_level')) ?>" />
+		…
+		<label for="job_level">Job Level:</label>
+		<select name="job_level_op">
+			<option value="eq"<?php if (($job_level_op=$params->get('job_level_op')) == 'eq') echo ' selected="selected"' ?>>is equal to</option>
+			<option value="gt"<?php if ($job_level_op == 'gt') echo ' selected="selected"' ?>>is greater than</option>
+			<option value="lt"<?php if ($job_level_op == 'lt') echo ' selected="selected"' ?>>is less than</option>
+		</select>
+		<input type="text" name="job_level" id="job_level" value="<?php echo htmlspecialchars($params->get('job_level')) ?>" />
+		…
+		<label for="zeny">Zeny:</label>
+		<select name="zeny_op">
+			<option value="eq"<?php if (($zeny_op=$params->get('zeny_op')) == 'eq') echo ' selected="selected"' ?>>is equal to</option>
+			<option value="gt"<?php if ($zeny_op == 'gt') echo ' selected="selected"' ?>>is greater than</option>
+			<option value="lt"<?php if ($zeny_op == 'lt') echo ' selected="selected"' ?>>is less than</option>
+		</select>
+		<input type="text" name="zeny" id="zeny" value="<?php echo htmlspecialchars($params->get('zeny')) ?>" />
+	</p>
+	<p>
+		<label for="guild">Guild:</label>
+		<input type="text" name="guild" id="guild" value="<?php echo htmlspecialchars($params->get('guild')) ?>" />
+		…
+		<label for="partner">Partner:</label>
+		<input type="text" name="partner" id="partner" value="<?php echo htmlspecialchars($params->get('partner')) ?>" />
+		…
+		<label for="mother">Mother:</label>
+		<input type="text" name="mother" id="mother" value="<?php echo htmlspecialchars($params->get('mother')) ?>" />
+		…
+		<label for="father">Father:</label>
+		<input type="text" name="father" id="father" value="<?php echo htmlspecialchars($params->get('father')) ?>" />
+		…
+		<label for="child">Child:</label>
+		<input type="text" name="child" id="child" value="<?php echo htmlspecialchars($params->get('child')) ?>" />
+	</p>
+	<p>	
+		<label for="online">Online Status:</label>
+		<select name="online" id="online">
+			<option value=""<?php if (!($online=$params->get('online'))) echo ' selected="selected"' ?>>All</option>
+			<option value="on"<?php if ($online == 'on') echo ' selected="selected"' ?>>Online</option>
+			<option value="off"<?php if ($online == 'off') echo ' selected="selected"' ?>>Offline</option>
+		</select>
+		…
+		<label for="slot">Slot Number:</label>
+		<select name="slot_op">
+			<option value="eq"<?php if (($slot_op=$params->get('slot_op')) == 'eq') echo ' selected="selected"' ?>>is equal to</option>
+			<option value="gt"<?php if ($slot_op == 'gt') echo ' selected="selected"' ?>>is greater than</option>
+			<option value="lt"<?php if ($slot_op == 'lt') echo ' selected="selected"' ?>>is less than</option>
+		</select>
+		<input type="text" name="slot" id="slot" value="<?php echo htmlspecialchars($params->get('slot')) ?>" />
+		
+		<input type="submit" value="Search" />
+		<input type="button" value="Reset" onclick="reload()" />
+	</p>
+</form>
 <?php if ($characters): ?>
 <?php echo $paginator->infoText() ?>
 <table class="vertical-table">
