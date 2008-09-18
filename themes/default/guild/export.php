717e33a737c5b7376b21bf4141e617487fd5bc66
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-18 15:40:02 +0000

    Added some styling.

--- themes/default/guild/export.php
+++ themes/default/guild/export.php
@@ -3,10 +3,10 @@
 <p>Please select the servers for which you would like to have the guild emblems exported as an archived ZIP file.</p>
 <form action="<?php echo $this->url ?>" method="post">
 	<?php foreach ($serverNames as $serverName): ?>
-	<p style="margin: 0; padding: 0"><label>
+	<p class="emblem-server"><label>
 		&raquo;
 		<input type="checkbox" name="server[]" checked="checked" value="<?php echo htmlspecialchars($serverName) ?>" />
-		<?php echo htmlspecialchars($serverName) ?>
+		<span><?php echo htmlspecialchars($serverName) ?></span>
 	</label></p>
 	<?php endforeach ?>
 	<button type="submit" class="submit_button">Export</button>
