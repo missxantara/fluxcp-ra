0f8ef969bdaa4f7a43f1f819e2852981faa18a87
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-18 02:32:08 +0000

    Added a bunch of code that confirms whether or not the provided CP database users have the necessary schema privileges to proceed with installation. These include SELECT, UPDATE, INSERT, DELETE, DROP and ALTER. If the user does not have all the necessary privileges, then an exception will be thrown and installation with Flux cannot proceed.

--- modules/install/index.php
+++ modules/install/index.php
@@ -5,13 +5,73 @@ if ($session->installerAuth) {
 	if ($params->get('logout')) {
 		$session->setInstallerAuthData(false);
 	}
-	elseif ($params->get('update_all')) {
-		$installer->updateAll();
+	else {
+		$requiredMySqlVersion = '5.0';
+		$requiredDbPrivileges = array('SELECT', 'UPDATE', 'INSERT', 'DELETE', 'DROP', 'ALTER');
 		
-		if (!$installer->updateNeeded()) {
-			$session->setMessageData('Updates have been installed.');
-			$session->setInstallerAuthData(false);
-			$this->redirect();
+		foreach (Flux::$loginAthenaGroupRegistry as $serverName => $loginAthenaGroup) {
+			$sth = $loginAthenaGroup->connection->getStatement("SELECT VERSION() AS mysql_version, CURRENT_USER() AS mysql_user");
+			$sth->execute();
+			
+			$res = $sth->fetch();
+			if (!$res || version_compare($res->mysql_version, $requiredMySqlVersion, '<')) {
+				$message  = "MySQL version $requiredMySqlVersion or greater is required for Flux.";
+				$message .= $res ? " You are running version {$res->mysql_version}" : "You are running an unknown version";
+				$message .= " on the server '$serverName'"; 
+				throw new Flux_Error($message);
+			}
+			
+			list($user, $host) = explode('@', $res->mysql_user);
+			$myUser = "'$user'@'$host'";
+			$bind   = array($myUser, $loginAthenaGroup->loginDatabase);
+			$sql    = "SELECT PRIVILEGE_TYPE AS priv, TABLE_SCHEMA AS dbname FROM INFORMATION_SCHEMA.SCHEMA_PRIVILEGES ";
+			$sql   .= "WHERE GRANTEE = ? AND (TABLE_SCHEMA = ?";
+			
+			foreach ($loginAthenaGroup->athenaServers as $athenaServer) {
+				$sql   .= " OR TABLE_SCHEMA = ?";
+				$bind[] = $athenaServer->charMapDatabase;
+			}
+			$sql .= ")";
+			$sth  = $loginAthenaGroup->connection->getStatement($sql);
+			$sth->execute($bind);
+			
+			$privs = $sth->fetchAll();
+			if (!$privs) {
+				throw new Flux_Error('Unable to get database privileges from the INFORMATION_SCHEMA.SCHEMA_PRIVILEGES view.');
+			}
+			
+			$existingPrivs = array();
+			foreach ($privs as $priv) {
+				if (in_array($priv->priv, $requiredDbPrivileges)) {
+					if (!array_key_exists($priv->dbname, $existingPrivs)) {
+						$existingPrivs[$priv->dbname] = array();
+					}
+					
+					$existingPrivs[$priv->dbname][] = $priv->priv;
+				}
+			}
+			
+			foreach ($existingPrivs as $dbName => $privArr) {
+				$missingPrivs = array_diff($requiredDbPrivileges, $privArr);
+				
+				if (count($missingPrivs)) {
+					$message  = "Flux has detected that the $myUser user lacks all the necessary privileges on the '$dbName' database. ";
+					$message .= sprintf("Please ensure you have %s privileges. Currently you are missing the %s privilege(s).",
+						implode(', ', $requiredDbPrivileges), implode(', ', $missingPrivs));
+					
+					throw new Flux_Error($message);
+				}
+			}
+		}
+		
+		if ($params->get('update_all')) {
+			$installer->updateAll();
+
+			if (!$installer->updateNeeded()) {
+				$session->setMessageData('Updates have been installed.');
+				$session->setInstallerAuthData(false);
+				$this->redirect();
+			}
 		}
 	}
 }
