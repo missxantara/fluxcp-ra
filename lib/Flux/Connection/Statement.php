b28472165d87d2912d7299412d5bbf4928a3477c
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-22 04:56:24 +0000

    Make error log handler static.

--- lib/Flux/Connection/Statement.php
+++ lib/Flux/Connection/Statement.php
@@ -3,12 +3,15 @@ require_once 'Flux/LogFile.php';
 
 class Flux_Connection_Statement {
 	public $stmt;
-	private $errorLog;
+	private static $errorLog;
 	
 	public function __construct(PDOStatement $stmt)
 	{
-		$this->stmt     = $stmt;
-		$this->errorLog = new Flux_LogFile(FLUX_DATA_DIR.'/logs/mysql/errors/'.date('Ymd').'.log', 'a');
+		$this->stmt = $stmt;
+		
+		if (!self::$errorLog) {
+			self::$errorLog = new Flux_LogFile(FLUX_DATA_DIR.'/logs/mysql/errors/'.date('Ymd').'.log', 'a');
+		}
 	}
 	
 	public function execute(array $inputParameters = array())
@@ -16,7 +19,7 @@ class Flux_Connection_Statement {
 		$res = $this->stmt->execute($inputParameters);
 		if ((int)$this->stmt->errorCode()) {
 			$info = $this->stmt->errorInfo();
-			$this->errorLog->puts('[SQLSTATE=%s] Err %s: %s', $info[0], $info[1], $info[2]);
+			self::$errorLog->puts('[SQLSTATE=%s] Err %s: %s', $info[0], $info[1], $info[2]);
 		}
 		return $res;
 	}
