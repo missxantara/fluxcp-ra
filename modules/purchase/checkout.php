ed5549397fa466a4454ac3479c152383669bb9ac
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-24 10:36:18 +0000

    Added checkout page.

---
+++ modules/purchase/checkout.php
@@ -0,0 +1,63 @@
+<?php
+if (!defined('FLUX_ROOT')) exit;
+
+$this->loginRequired();
+
+if ($server->cart->isEmpty()) {
+	$session->setMessageData('Your cart is currently empty.');
+	$this->redirect($this->url('purchase'));
+}
+elseif (!$server->cart->hasFunds()) {
+	$session->setMessageData('You do not have sufficient funds to make this purchase!');
+	$this->redirect($this->url('purchase'));
+}
+
+$items = $server->cart->getCartItems();
+
+if (count($_POST) && $params->get('process')) {
+	$redeemTable = Flux::config('FluxTables.RedemptionTable');
+	$creditTable = Flux::config('FluxTables.CreditsTable');
+	$deduct      = 0;
+	
+	$sql  = "INSERT INTO {$server->charMapDatabase}.$redeemTable (nameid, quantity, cost, account_id, char_id, redeemed, redemption_date) ";
+	$sql .= "VALUES (?, ?, ?, ?, NULL, 0, NULL)";
+	$sth  = $server->connection->getStatement($sql);
+	
+	foreach ($items as $item) {
+		$res = $sth->execute(array(
+			$item->shop_item_nameid,
+			$item->shop_item_qty,
+			$item->shop_item_cost,
+			$session->account->account_id,
+		));
+		
+		if ($res) {
+			$deduct += $item->shop_item_cost;
+		}
+	}
+	
+	$sql = "UPDATE {$server->loginDatabase}.$creditTable SET balance = balance - ? WHERE account_id = ?";
+	$sth = $server->connection->getStatement($sql);
+	$sth->execute(array($deduct, $session->account->account_id));
+	
+	if ($res) {
+		if (!$deduct) {
+			$server->cart->clear();
+			$session->setMessageData('Failed to purchase all of the items in your cart!');
+		}
+		elseif ($deduct != $server->cart->getTotal()) {
+			$server->cart->clear();
+			$session->setMessageData('Items have been purchased, however, some failed (your credits are still there.)');
+		}
+		else {
+			$server->cart->clear();
+			$session->setMessageData('Items have been purchased.  You may redeem them from the Redemption NPC.');
+		}
+	}
+	else {
+		$session->setMessageData('Purchase went bad, contact an admin!');
+	}
+	
+	$this->redirect();
+}
+?>
