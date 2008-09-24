7b25f143754138b6a5c7f41b16866ae0ab5a978a
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-24 02:22:42 +0000

    Changed some wording in the cart empty failure message.

--- modules/purchase/clear.php
+++ modules/purchase/clear.php
@@ -9,7 +9,7 @@ if ($server->cart && $server->cart->clear()) {
 	$session->setMessageData("Your cart has been emptied.");
 }
 else {
-	$session->setMessageData("Couldn't empty your cart.");
+	$session->setMessageData("Couldn't empty your cart, maybe it's already empty.");
 }
 
 $this->redirect($this->url('purchase'));
