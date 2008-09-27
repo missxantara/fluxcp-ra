8b3ba3d01d751a8ae152a0df8f38701087ab26ea
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-09-27 17:26:37 +0000

    Renamed some CSS classes to be more generic for re-use.

--- themes/default/account/changepass.php
+++ themes/default/account/changepass.php
@@ -6,8 +6,8 @@
 	<p>Please enter your current password, then enter the new password you would like to use and re-enter it to confirm.</p>
 <?php endif ?>
 <br />
-<form action="<?php echo $this->urlWithQs ?>" method="post" class="change-password-form">
-	<table class="change-password-table">
+<form action="<?php echo $this->urlWithQs ?>" method="post" class="generic-form">
+	<table class="generic-form-table">
 		<tr>
 			<th><label for="currentpass">Current Password</label></th>
 			<td><input type="password" name="currentpass" id="currentpass" value="" /></td>
