422638f164eadfbcc2d856f082176d07efa8c60f
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-10-01 08:58:19 +0000

    Re-styled some more and added some informational text.

--- themes/default/server/status.php
+++ themes/default/server/status.php
@@ -1,7 +1,13 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 <h2>Server Status</h2>
-<table id="server_status">
+<p>
+	Understanding the online and offline status of each server can help you understand how an issue can relate to your problem.
+	For example, if the login server is offline it means that you won't be able to log into the game.
+	The character server and map servers are necessary for the actual gameplay past the point of logging in.
+</p>
 <?php foreach ($serverStatus as $privServerName => $gameServers): ?>
+<h3>Server Status for <?php echo htmlspecialchars($privServerName) ?></h3>
+<table id="server_status">
 	<tr>
 		<td class="status">Server</td>
 		<td class="status">Login Server</td>
@@ -18,5 +24,5 @@
 		<td class="status"><?php echo $gameServer['playersOnline'] ?></td>
 	</tr>
 	<?php endforeach ?>
-<?php endforeach ?>
 </table>
+<?php endforeach ?>
