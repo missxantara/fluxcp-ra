35c00bcd0c8b77cfe90e587e0fb2f0fb4c5829f6
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-10-02 02:58:33 +0000

    Added "Guild Ranking" page.

--- modules/ranking/guild.php
+++ modules/ranking/guild.php
@@ -2,4 +2,20 @@
 if (!defined('FLUX_ROOT')) exit;
 
 $this->loginRequired();
+
+$title = 'Guild Ranking';
+
+$col  = "g.guild_id, g.name, g.guild_lv, g.exp, ";
+$col .= "(SELECT COUNT(char_id) FROM {$server->charMapDatabase}.`char` WHERE `char`.guild_id = g.guild_id) AS members";
+
+$sql  = "SELECT $col FROM {$server->charMapDatabase}.guild AS g ";
+$sql .= "LEFT JOIN {$server->charMapDatabase}.`char` AS ch ON ch.char_id = g.char_id ";
+$sql .= "LEFT JOIN {$server->loginDatabase}.login ON login.account_id = ch.account_id ";
+$sql .= "WHERE login.level < ? ";
+$sql .= "ORDER BY g.guild_lv DESC, g.exp DESC, members DESC, g.max_member DESC, g.next_exp ASC ";
+$sql .= "LIMIT ".(int)Flux::config('GuildRankingLimit');
+$sth  = $server->connection->getStatement($sql);
+
+$sth->execute(array((int)Flux::config('RankingHideLevel')));
+$guilds = $sth->fetchAll();
 ?>
--- themes/default/ranking/guild.php
+++ themes/default/ranking/guild.php
@@ -1,2 +1,30 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 <h2>Guild Ranking</h2>
+<h3>Top <?php echo number_format($limit=(int)Flux::config('CharRankingLimit')) ?> Guilds</h3>
+<?php if ($guilds): ?>
+	<table class="horizontal-table">
+		<tr>
+			<th>Rank</th>
+			<th colspan="2">Guild Name</th>
+			<th>Level</th>
+			<th>Members</th>
+			<th>Experience</th>
+		</tr>
+		<?php for ($i = 0; $i < $limit; ++$i): ?>
+		<tr>
+			<td align="right"><?php echo number_format($i + 1) ?></td>
+			<?php if (isset($guilds[$i])): ?>
+			<td width="24"><img src="<?php echo $this->emblem($guilds[$i]->guild_id) ?>" /></td>
+			<td><strong><?php echo htmlspecialchars($guilds[$i]->name) ?></strong></td>
+			<td><?php echo number_format($guilds[$i]->guild_lv) ?></td>
+			<td><?php echo number_format($guilds[$i]->members) ?></td>
+			<td><?php echo number_format($guilds[$i]->exp) ?></td>
+			<?php else: ?>
+			<td colspan="8"></td>
+			<?php endif ?>
+		</tr>
+		<?php endfor ?>
+	</table>
+<?php else: ?>
+<p>No guilds found. <a href="javascript:history.go(-1)">Go back</a>.</p>
+<?php endif ?>
