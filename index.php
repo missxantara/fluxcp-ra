a8148198e429cdd2e7ccf75b0b57be0f48aff2c6
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-10-05 12:45:23 +0000

    Bumped minimum version requirement to 5.2.x

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-if (version_compare(PHP_VERSION, '5.1', '<')) {
+if (version_compare(PHP_VERSION, '5.2', '<')) {
 	echo '<h2>Error</h2>';
-	echo '<p>PHP 5.1 or higher is required to use Flux Control Panel.</p>';
+	echo '<p>PHP 5.2 or higher is required to use Flux Control Panel.</p>';
 	echo '<p>You are running '.PHP_VERSION.'</p>';
 	exit;
 }
