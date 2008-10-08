8e55e028ee3fc6ad4d7461a66addc0af78c449b6
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-10-08 12:13:57 +0000

    Added comments for 'ChangeSlot' and 'ModifyIpBan' feature access levels.

--- config/access.php
+++ config/access.php
@@ -148,8 +148,8 @@ return array(
 		'ModifyCharPrefs'    => AccountLevel::ADMIN,  // Ability to modify another person's character preferences.
 		'IgnoreHiddenPref'   => AccountLevel::HELPER, // Ability to see users on "Who's Online" page, hidden or not.
 		'IgnoreHiddenPref2'  => AccountLevel::HELPER, // Ability to see users on "Who's Online" page, hidden by app config or not.
-		'ChangeSlot'         => AccountLevel::LOWGM,  //
-		'ModifyIpBan'        => AccountLevel::ADMIN,  //
+		'ChangeSlot'         => AccountLevel::LOWGM,  // Level required to change another character's slot.
+		'ModifyIpBan'        => AccountLevel::ADMIN,  // Level required to modify an existing IP ban.
 		'HideFromZenyRank'   => AccountLevel::NORMAL, // Ability to set "Hide from zeny ranking" pref.
 		'SeeItemDbScripts'   => AccountLevel::ANYONE, // Ability to see item_db scripts in view page.
 		'SeeItemDb2Scripts'  => AccountLevel::ADMIN,  // Ability to see item_db2 scripts in view page.
