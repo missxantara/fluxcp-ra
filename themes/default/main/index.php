37d3c0d22b790bc64c826138d155119c1630766d
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-10-22 14:15:19 +0000

    Created a more useful main page.  This is likely to cause conflicts during update for anyone who has customized their main page.  Simply keep your copy and resolve the conflict since this change only puts more useful information for new users.

--- themes/default/main/index.php
+++ themes/default/main/index.php
@@ -1,7 +1,9 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
-<h2>Main Page</h2>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis mauris. Sed lacus enim, consectetuer sit amet, viverra id, tincidunt vitae, mauris. Aliquam adipiscing, turpis eu varius ornare, lacus nisl consequat lectus, vel pharetra augue arcu a tellus. Vivamus ante. Sed placerat. Pellentesque vel quam a metus malesuada rhoncus. Ut viverra auctor diam. Aliquam sit amet libero. Etiam leo elit, feugiat ut, convallis mattis, accumsan eget, risus. Nullam facilisis molestie justo. In hac habitasse platea dictumst. Aliquam libero lorem, dignissim in, euismod sed, tristique sed, dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc sed odio eu risus bibendum venenatis. Pellentesque ac tortor. Curabitur luctus augue in lectus.</p>
-<p>Sed fermentum. Etiam mollis pretium ante. Etiam posuere massa ut orci. Proin egestas nulla ut nulla. Maecenas nec purus. Proin enim. Aenean metus erat, interdum sit amet, viverra in, auctor eget, nisl. Sed sodales, lorem non scelerisque pretium, nisi magna volutpat lectus, vel tincidunt nibh nibh nec lectus. Praesent ut sapien in nisi iaculis fermentum. Nunc molestie vestibulum est.</p>
-<p>Praesent adipiscing enim eget tortor. Quisque orci. Sed dolor metus, ultrices sed, mattis quis, ultricies id, metus. Donec ut augue eget eros luctus blandit. Fusce sit amet ipsum. Fusce mollis laoreet velit. Quisque neque justo, semper a, accumsan sit amet, malesuada viverra, arcu. Nunc viverra turpis. Suspendisse quis augue eget justo tempor cursus. Pellentesque pulvinar orci ut arcu. Fusce accumsan. Pellentesque sollicitudin sem in pede consectetuer consequat. Fusce et metus a eros commodo consequat.</p>
-<p>Donec quis orci. Donec a ligula. Aenean sed purus. Ut hendrerit elit a justo varius accumsan. Aliquam volutpat placerat diam. Fusce condimentum, orci nec venenatis aliquet, turpis libero condimentum urna, imperdiet semper libero purus a enim. Morbi non nulla. Aliquam lorem lorem, mollis sit amet, tempus eget, scelerisque eu, magna. Integer tincidunt vehicula purus. Sed ultricies lorem nec tellus.</p>
-<p>Sed blandit convallis nulla. Mauris vehicula lobortis mauris. In eu elit iaculis urna rhoncus pharetra. Vestibulum turpis. Fusce sed est nec nibh mollis eleifend. Nulla facilisi. Curabitur commodo ultricies est. Vestibulum est. In sit amet eros quis urna dignissim viverra. Vivamus congue lorem a odio. Sed ut magna. Duis dolor.</p>
+<h2><?php echo htmlspecialchars(Flux::config('SiteTitle')) ?></h2>
+<p><strong>If you are seeing this page, it's likely that you've successfully installed Flux.</strong></p>
+<p>Would you like to change this page? Well, here's how you can:</p>
+<ol>
+	<li><p class="green">Open <code><?php echo __FILE__ ?></code> in your text editor.</p></li>
+	<li><p class="green">Edit the file from your editor and save your changes.</p></li>
+</ol>
+<p style="text-align: right"><strong><em>Thanks for using Flux!</em></strong></p>
