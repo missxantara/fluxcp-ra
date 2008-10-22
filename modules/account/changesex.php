f06c80424813c7662d8e21372d1bff8b08b7a98f
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-10-22 06:37:04 +0000

    Prevent gender changes for accounts that possess a character that is one of the following job classes: Bard, Clown, Gypsy or Dancer.

--- modules/account/changesex.php
+++ modules/account/changesex.php
@@ -3,7 +3,9 @@ if (!defined('FLUX_ROOT')) exit;
 
 $this->loginRequired();
 
-$cost = +(int)Flux::config('ChargeGenderChange');
+$cost     = +(int)Flux::config('ChargeGenderChange');
+$jobIndex = Flux::config('JobClassIndex');
+$badJobs  = array('Bard', 'Clown', 'Gypsy', 'Dancer');
 
 if ($cost && $session->account->balance < $cost) {
 	$hasNecessaryFunds = false;
@@ -17,23 +19,54 @@ if (count($_POST)) {
 		$this->deny();
 	}
 	
-	$sex = $session->account->sex == 'M' ? 'F' : 'M';
-	$sql = "UPDATE {$server->loginDatabase}.login SET sex = ? WHERE account_id = ?";
-	$sth = $server->connection->getStatement($sql);
-	
-	$sth->execute(array($sex, $session->account->account_id));
-	
-	$changeTimes = (int)$session->loginServer->getPref($session->account->account_id, 'NumberOfGenderChanges');
-	$session->loginServer->setPref($session->account->account_id, 'NumberOfGenderChanges', $changeTimes + 1);
-	
-	if ($cost) {
-		$session->loginServer->depositCredits($session->account->account_id, -$cost);
-		$session->setMessageData(sprintf(Flux::message('GenderChanged'), $cost));
-	}
-	else {
-		$session->setMessageData(Flux::message('GenderChangedForFree'));
+	$classes = array();
+	foreach ($session->loginAthenaGroup->athenaServers as $athenaServer) {
+		$sql = "SELECT `class` FROM {$athenaServer->charMapDatabase}.`char` WHERE account_id = ?";
+		$sth = $athenaServer->connection->getStatement($sql);
+		$sth->execute(array($session->account->account_id));
+		$chars = $sth->fetchAll();
+		
+		if ($chars) {
+			foreach ($chars as $char) {
+				$classes[] = $char->class;
+			}
+		}
 	}
 	
-	$this->redirect($this->url('account', 'view'));
+	$bad = array();
+	foreach ($badJobs as $badJob) {
+		if ($index=$jobIndex->get($badJob)) {
+			$bad[] = $index;
+		}
+	}
+	
+	foreach ($classes as $class) {
+		if (in_array($class, $bad)) {
+			$errorMessage  = 'You cannot change your gender if any of your characters is a: ';
+			$errorMessage .= implode(', ', $badJobs);
+			break;
+		}
+	}
+	
+	if (empty($errorMessage)) {
+		$sex = $session->account->sex == 'M' ? 'F' : 'M';
+		$sql = "UPDATE {$server->loginDatabase}.login SET sex = ? WHERE account_id = ?";
+		$sth = $server->connection->getStatement($sql);
+
+		$sth->execute(array($sex, $session->account->account_id));
+
+		$changeTimes = (int)$session->loginServer->getPref($session->account->account_id, 'NumberOfGenderChanges');
+		$session->loginServer->setPref($session->account->account_id, 'NumberOfGenderChanges', $changeTimes + 1);
+
+		if ($cost) {
+			$session->loginServer->depositCredits($session->account->account_id, -$cost);
+			$session->setMessageData(sprintf(Flux::message('GenderChanged'), $cost));
+		}
+		else {
+			$session->setMessageData(Flux::message('GenderChangedForFree'));
+		}
+
+		$this->redirect($this->url('account', 'view'));
+	}
 }
 ?>
