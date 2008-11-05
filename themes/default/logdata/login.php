4127339f892d4f26989fb37823746455d8f09d67
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-11-05 05:36:55 +0000

    Check for additional privilege "ViewAccount" before displaying a link to the acocunt.

--- themes/default/logdata/login.php
+++ themes/default/logdata/login.php
@@ -43,7 +43,7 @@
 		<td align="right"><?php echo htmlspecialchars($this->formatDateTime($login->time)) ?></td>
 		<td><?php echo htmlspecialchars($login->ip) ?></td>
 		<td>
-			<?php if ($login->account_id && $auth->actionAllowed('account', 'view')): ?>
+			<?php if ($login->account_id && $auth->actionAllowed('account', 'view') && $auth->allowedToViewAccount): ?>
 				<?php echo $this->linkToAccount($login->account_id, $login->user) ?>
 			<?php else: ?>
 				<?php echo htmlspecialchars($login->user) ?>
