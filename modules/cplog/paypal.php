fe9b39dd92f9d53ce713a3d12d899fe097e20ea7
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-11-06 09:25:46 +0000

    * Added transaction search support. * Fixed field ambiguities in column sorting.

--- modules/cplog/paypal.php
+++ modules/cplog/paypal.php
@@ -9,24 +9,98 @@ $txnLogTable = Flux::config('FluxTables.TransactionTable');
 $sqlpartial  = "{$server->loginDatabase}.{$txnLogTable} AS p ";
 $sqlpartial .= "LEFT OUTER JOIN {$server->loginDatabase}.login AS l ON p.account_id = l.account_id ";
 $sqlpartial .= "LEFT OUTER JOIN {$server->loginDatabase}.$txnLogTable AS pp ON pp.txn_id = p.parent_txn_id ";
-$sqlpartial .= "WHERE (p.server_name = ? OR p.server_name IS NULL OR p.server_name = '')";
+$sqlpartial .= "WHERE (p.server_name = ? OR p.server_name IS NULL OR p.server_name = '') ";
+
+$bind            = array($session->loginAthenaGroup->serverName);
+$opMapping       = array('eq' => '=', 'gt' => '>', 'lt' => '<');
+$opValues        = array_keys($opMapping);
+$txnID           = trim($params->get('txn_id'));
+$parentTxnID     = trim($params->get('parent_txn_id'));
+$paymentStatus   = trim($params->get('status'));
+$emailAddress    = trim($params->get('email'));
+$amount          = $params->get('amount');
+$amountOp        = $params->get('amount_op');
+$credits         = $params->get('credits');
+$creditsOp       = $params->get('credits_op');
+$account         = $params->get('account');
+$processedAfter  = $params->get('processed_after_date');
+$processedBefore = $params->get('processed_before_date');
+$receivedAfter   = $params->get('received_after_date');
+$receivedBefore  = $params->get('received_before_date');
+
+if ($txnID) {
+	$sqlpartial .= 'AND p.txn_id = ? ';
+	$bind[]      = $txnID;
+}
+
+if ($parentTxnID) {
+	$sqlpartial .= 'AND p.parent_txn_id = ? ';
+	$bind[]      = $parentTxnID;
+}
+
+if ($paymentStatus) {
+	$sqlpartial .= 'AND p.payment_status LIKE ?';
+	$bind[]      = "%$paymentStatus%";
+}
+
+if ($emailAddress) {
+	$sqlpartial .= 'AND p.payer_email LIKE ?';
+	$bind[]      = "%$emailAddress%";
+}
+
+if ($account) {
+	$sqlpartial .= 'AND l.userid LIKE ?';
+	$bind[]      = "%$account%";
+}
+
+if (in_array($amountOp, $opValues) && trim($amount) != '') {
+	$op          = $opMapping[$amountOp];
+	$sqlpartial .= "AND CAST(p.mc_gross AS SIGNED) $op ? ";
+	$bind[]      = $amount;
+}
+
+if (in_array($creditsOp, $opValues) && trim($credits) != '') {
+	$op          = $opMapping[$creditsOp];
+	$sqlpartial .= "AND CAST(p.credits AS UNSIGNED) $op ? ";
+	$bind[]      = $credits;
+}
+
+if ($processedAfter) {
+	$sqlpartial .= 'AND p.process_date >= ? ';
+	$bind[]      = $processedAfter;
+}
+
+if ($processedBefore) {
+	$sqlpartial .= 'AND p.process_date <= ? ';
+	$bind[]      = $processedBefore;
+}
+
+if ($receivedAfter) {
+	$sqlpartial .= 'AND p.payment_date >= ? ';
+	$bind[]      = $receivedAfter;
+}
+
+if ($receivedBefore) {
+	$sqlpartial .= 'AND p.payment_date <= ? ';
+	$bind[]      = $receivedBefore;
+}
 
 $sth = $server->connection->getStatement("SELECT COUNT(p.id) AS total FROM $sqlpartial");
-$sth->execute(array($session->loginAthenaGroup->serverName));
+$sth->execute($bind);
 
 $paginator = $this->getPaginator($sth->fetch()->total);
 $paginator->setSortableColumns(
 	array(
-		'txn_id',
-		'parent_txn_id',
-		'process_date' => 'DESC',
-		'payment_date',
-		'payment_status',
-		'payer_email',
-		'mc_gross',
-		'credits',
-		'server_name',
-		'userid'
+		'p.txn_id',
+		'p.parent_txn_id',
+		'p.process_date' => 'DESC',
+		'p.payment_date',
+		'p.payment_status',
+		'p.payer_email',
+		'p.mc_gross',
+		'p.credits',
+		'p.server_name',
+		'l.userid'
 	)
 );
 
@@ -37,6 +111,6 @@ $sql  = "SELECT $col FROM $sqlpartial";
 $sql  = $paginator->getSQL($sql);
 $sth  = $server->connection->getStatement($sql);
 
-$sth->execute(array($session->loginAthenaGroup->serverName));
+$sth->execute($bind);
 $transactions = $sth->fetchAll();
 ?>
