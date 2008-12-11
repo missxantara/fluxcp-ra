7027a9a46642375876b1563d3dbe2c340e9d0eab
shugotenshi <shugotenshi@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2008-12-11 05:37:29 +0000

    * Fixed an infinite redirect issue. * Fixed a security hole that would have otherwise been dangerous when the redirect issue got fixed (the security hole was being protected by the redirect bug, oddly).

--- modules/character/changeslot.php
+++ modules/character/changeslot.php
@@ -10,78 +10,76 @@ if (!$charID) {
 }
 
 $char = $server->getCharacter($charID);
-if ($char) {
-	$title = "Change Slot for {$char->name}";
-	
-	if ($char->account_id != $session->account->account_id && !$auth->allowedToChangeSlot) {
+if (!$char || ($char->account_id != $session->account->account_id && !$auth->allowedToChangeSlot)) {
+	$this->deny();
+}
+
+$title = "Change Slot for {$char->name}";
+
+if ($char->online) {
+	$session->setMessageData("Cannot change {$char->name}'s slot.  He/she is currenty online.");
+	$this->redirect();
+}
+
+if (count($_POST)) {
+	if (!$params->get('changeslot')) {
 		$this->deny();
 	}
 	
-	if ($char->online) {
-		$session->setMessageData("Cannot change {$char->name}'s slot.  He/she is currenty online.");
-		$this->redirect();
-	}
+	$slot = (int)$params->get('slot');
 	
-	if (count($_POST)) {
-		if (!$params->get('changeslot')) {
-			$this->deny();
-		}
+	if ($slot > $server->maxCharSlots) {
+		$errorMessage = "Slot number must not be greater than {$server->maxCharSlots}.";
+	}
+	elseif ($slot < 1) {
+		$errorMessage = 'Slot number must be a number greater than zero.';
+	}
+	elseif ($slot === (int)$char->char_num+1) {
+		$errorMessage = 'Please choose a different slot.';
+	}
+	else {
+		$sql  = "SELECT char_id, name, online FROM {$server->charMapDatabase}.`char` AS ch ";
+		$sql .= "WHERE account_id = ? AND char_num = ? AND char_id != ?";
+		$sth  = $server->connection->getStatement($sql);
 		
-		$slot = (int)$params->get('slot');
+		$sth->execute(array($char->account_id, $slot-1, $charID));
 		
-		if ($slot > $server->maxCharSlots) {
-			$errorMessage = "Slot number must not be greater than {$server->maxCharSlots}.";
-		}
-		elseif ($slot < 1) {
-			$errorMessage = 'Slot number must be a number greater than zero.';
-		}
-		elseif ($slot === (int)$char->char_num+1) {
-			$errorMessage = 'Please choose a different slot.';
-		}
-		else {
-			$sql  = "SELECT char_id, name, online FROM {$server->charMapDatabase}.`char` AS ch ";
-			$sql .= "WHERE account_id = ? AND char_num = ? AND char_id != ?";
-			$sth  = $server->connection->getStatement($sql);
-			
-			$sth->execute(array($char->account_id, $slot-1, $charID));
-			
-			$otherChar = $sth->fetch();
-			
-			if ($otherChar) {
-				if ($otherChar->online) {
-					$errorMessage = "{$otherChar->name} is using that slot, and is currently online.";
-				}
-				else {
-					$sql  = "UPDATE {$server->charMapDatabase}.`char` SET `char`.char_num = ?";
-					$sql .= "WHERE `char`.char_id = ?";
-					$sth  = $server->connection->getStatement($sql);
-					
-					$sth->execute(array($char->char_num, $otherChar->char_id));
-				}
+		$otherChar = $sth->fetch();
+		
+		if ($otherChar) {
+			if ($otherChar->online) {
+				$errorMessage = "{$otherChar->name} is using that slot, and is currently online.";
 			}
-			
-			if (empty($errorMessage)) {
+			else {
 				$sql  = "UPDATE {$server->charMapDatabase}.`char` SET `char`.char_num = ?";
 				$sql .= "WHERE `char`.char_id = ?";
 				$sth  = $server->connection->getStatement($sql);
 				
-				$sth->execute(array($slot-1, $charID));
-				
-				if ($otherChar) {
-					$otherNum = $char->char_num + 1;
-					$session->setMessageData("You have successfully swapped {$char->name}'s slot with {$otherChar->name} (#$otherNum and #$slot).");
-				}
-				else {
-					$session->setMessageData("You have successfully changed {$char->name}'s slot to #$slot.");
-				}
-				
-				$isMine = $char->account_id == $session->account->account_id;
-				if ($auth->actionAllowed('character', 'view') && ($isMine || $auth->allowedToViewCharacter)) {
-					$this->redirect($this->url('character', 'view', array('id' => $char->char_id)));
-				}
-				else {
-					$this->redirect();
-				}
+				$sth->execute(array($char->char_num, $otherChar->char_id));
+			}
+		}
+		
+		if (empty($errorMessage)) {
+			$sql  = "UPDATE {$server->charMapDatabase}.`char` SET `char`.char_num = ?";
+			$sql .= "WHERE `char`.char_id = ?";
+			$sth  = $server->connection->getStatement($sql);
+			
+			$sth->execute(array($slot-1, $charID));
+			
+			if ($otherChar) {
+				$otherNum = $char->char_num + 1;
+				$session->setMessageData("You have successfully swapped {$char->name}'s slot with {$otherChar->name} (#$otherNum and #$slot).");
+			}
+			else {
+				$session->setMessageData("You have successfully changed {$char->name}'s slot to #$slot.");
+			}
+			
+			$isMine = $char->account_id == $session->account->account_id;
+			if ($auth->actionAllowed('character', 'view') && ($isMine || $auth->allowedToViewCharacter)) {
+				$this->redirect($this->url('character', 'view', array('id' => $char->char_id)));
+			}
+			else {
+				$this->redirect();
 			}
 		}
 	}
