0825ba3e5a2d5359e02ebdd7344e6921c1cd4551
Paradox924X <Paradox924X@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2009-03-09 12:43:50 +0000

    Minor typo fix. No effect on functionality.

--- modules/character/resetpos.php
+++ modules/character/resetpos.php
@@ -16,17 +16,17 @@ if (!$char || ($char->account_id != $session->account->account_id && !$auth->all
 }
 
 $reset = $server->resetPosition($charID);
-if ($reset === -1) {
+if ($reset == -1) {
 	$message = sprintf(Flux::message('CantResetPosWhenOnline'), $char->name);
 }
-elseif ($reset === -2) {
-	$message = sprintf(FLux::message('CantResetFromCurrentMap'), $char->name);
+elseif ($reset == -2) {
+	$message = sprintf(Flux::message('CantResetFromCurrentMap'), $char->name);
 }
 elseif ($reset === true) {
-	$message = sprintf(FLux::message('ResetPositionSuccessful'), $char->name);
+	$message = sprintf(Flux::message('ResetPositionSuccessful'), $char->name);
 }
 else {
-	$message = sprintf(FLux::message('ResetPositionFailed'), $char->name);
+	$message = sprintf(Flux::message('ResetPositionFailed'), $char->name);
 }
 
 $session->setMessageData($message);
