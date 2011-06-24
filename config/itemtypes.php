9819d335f095771887e6a36b0e50e7b3bfc7f48c
Paradox924X <Paradox924X@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2011-06-24 16:13:25 +0000

    Disable unused item types by default.

--- config/itemtypes.php
+++ config/itemtypes.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	0 => 'Healing',
-	1 => 'Unknown',
+	//1 => 'Unknown',
 	2 => 'Usable',
 	3 => 'Etc',
 	4 => 'Weapon',
@@ -9,7 +9,7 @@ return array(
 	6 => 'Card',
 	7 => 'Pet Egg',
 	8 => 'Pet Armor',
-	9 => 'Unknown2',
+	//9 => 'Unknown2',
 	10 => 'Ammo',
 	11 => 'Delay Consume'
 )
