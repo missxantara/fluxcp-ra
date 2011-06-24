4c768c39060f7a5b6f5e99709435d137837f2c08
Paradox924X <Paradox924X@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2011-06-24 16:32:51 +0000

    Added purchase shop category for Cards as id 7 for cp_ table(s).

--- config/shopcategories.php
+++ config/shopcategories.php
@@ -4,12 +4,13 @@
 // category, so it knows which category the item belongs to.
 // You may add/remove categories from here as you see fit.
 return array(
-	0 => 'Headgears',     // Headgears category, most likely doesn't include wings.
-	1 => 'Wings',         // Wings category, though most of the time wings are headgear, this is more dedicated.
-	2 => 'Armors',        // Armors category, most likely doesn't include headgears.
-	3 => 'Weapons',       // Weapons category.
-	4 => 'Healing Items', // Good category to place healing items under, e.g., Yggdrasil Berries and the like.
-	5 => 'Pets',          // Some people may choose to sell pet eggs or pet-related items under here.
-	6 => 'Miscellaneous'  // Anything you can't really categories, you can put under this category.
+	0 => 'Headgears',      // Headgears category, most likely doesn't include wings.
+	1 => 'Wings',          // Wings category, though most of the time wings are headgear, this is more dedicated.
+	2 => 'Armors',         // Armors category, most likely doesn't include headgears.
+	3 => 'Weapons',        // Weapons category.
+	4 => 'Healing Items',  // Good category to place healing items under, e.g., Yggdrasil Berries and the like.
+	5 => 'Pets',           // Some people may choose to sell pet eggs or pet-related items under here.
+	6 => 'Miscellaneous',  // Anything you can't really categorize, you can put under this category.
+	7 => 'Cards'           // Cards category.
 );
 ?>
