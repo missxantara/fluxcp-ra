9ccb8ff6d73a7e12f44781235afad6481c923646
Paradox924X <Paradox924X@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2011-07-05 10:49:42 +0000

    Reordered guild ranking to show more appropriate results.

--- modules/ranking/guild.php
+++ modules/ranking/guild.php
@@ -10,7 +10,8 @@ $sql  = "SELECT $col FROM {$server->charMapDatabase}.guild AS g ";
 $sql .= "LEFT JOIN {$server->charMapDatabase}.`char` AS ch ON ch.char_id = g.char_id ";
 $sql .= "LEFT JOIN {$server->loginDatabase}.login ON login.account_id = ch.account_id ";
 $sql .= "WHERE login.level < ? ";
-$sql .= "ORDER BY g.guild_lv DESC, g.exp DESC, members DESC, g.average_lv DESC, g.max_member DESC, g.next_exp ASC ";
+$sql .= "ORDER BY g.guild_lv DESC, g.exp DESC, (g.average_lv + members) DESC, ";
+$sql .= "g.average_lv DESC, members DESC, g.max_member DESC, g.next_exp ASC ";
 $sql .= "LIMIT ".(int)Flux::config('GuildRankingLimit');
 $sth  = $server->connection->getStatement($sql);
 
