99a0ca6b6aed49c0105b7aefe71184b8e88bf285
Paradox924X <Paradox924X@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2012-03-18 03:29:02 +0000

    Modified guild ranking to use the greater of the current exp gained towards the next level, and the sum of devoted experience to a guild by its members (which is now of type uint64/bigint in eAthena and thus can grow to extremely large amounts, which is appropriate). The priority of order attributes on the ranking has not changed.

--- modules/ranking/guild.php
+++ modules/ranking/guild.php
@@ -3,7 +3,8 @@ if (!defined('FLUX_ROOT')) exit;
 
 $title = 'Guild Ranking';
 
-$col  = "g.guild_id, g.name, g.guild_lv, g.exp, g.average_lv, g.emblem_len, ";
+$col  = "g.guild_id, g.name, g.guild_lv, g.average_lv, g.emblem_len, ";
+$col .= "GREATEST(g.exp, (SELECT SUM(exp) FROM {$server->charMapDatabase}.guild_member WHERE guild_member.guild_id = g.guild_id)) AS exp, ";
 $col .= "(SELECT COUNT(char_id) FROM {$server->charMapDatabase}.`char` WHERE `char`.guild_id = g.guild_id) AS members, ";
 $col .= "(SELECT COUNT(castle_id) FROM {$server->charMapDatabase}.guild_castle WHERE guild_castle.guild_id = g.guild_id) AS castles";
 
@@ -11,7 +12,7 @@ $sql  = "SELECT $col FROM {$server->charMapDatabase}.guild AS g ";
 $sql .= "LEFT JOIN {$server->charMapDatabase}.`char` AS ch ON ch.char_id = g.char_id ";
 $sql .= "LEFT JOIN {$server->loginDatabase}.login ON login.account_id = ch.account_id ";
 $sql .= "WHERE login.level < ? ";
-$sql .= "ORDER BY g.guild_lv DESC, castles DESC, g.exp DESC, (g.average_lv + members) DESC, ";
+$sql .= "ORDER BY g.guild_lv DESC, castles DESC, exp DESC, (g.average_lv + members) DESC, ";
 $sql .= "g.average_lv DESC, members DESC, g.max_member DESC, g.next_exp ASC ";
 $sql .= "LIMIT ".(int)Flux::config('GuildRankingLimit');
 $sth  = $server->connection->getStatement($sql);
