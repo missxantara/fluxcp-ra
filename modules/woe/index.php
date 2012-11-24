9c97ccc128d628695e6c306d08ee53e84e872ea3
missxantara@gmail.com <missxantara@gmail.com@f2328f5b-46c4-c848-6f58-900bf11a61a7>
2012-11-24 19:50:21 +0000

    * Merged FluxCP revision 1125 by [Paradox] Changed list of day names to hardcoded list to reduce unnecessary processing.

--- modules/woe/index.php
+++ modules/woe/index.php
@@ -2,14 +2,7 @@
 if (!defined('FLUX_ROOT')) exit;
 
 $title     = Flux::message('WoeTitle');
-$sunday    = date('l', $sun=strtotime('Sunday'));
-$monday    = date('l', $mon=($sun+86400));
-$tuesday   = date('l', $tue=($mon+86400));
-$wednesday = date('l', $wed=($tue+86400));
-$thursday  = date('l', $thu=($wed+86400));
-$friday    = date('l', $fri=($thu+86400));
-$saturday  = date('l', $sat=($fri+86400));
-$dayNames  = array($sunday, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday);
+$dayNames  = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 $woeTimes  = array();
 
 foreach ($session->loginAthenaGroup->athenaServers as $athenaServer) {
